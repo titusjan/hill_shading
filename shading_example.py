64be3da9ffc2bf039d49b3fcdf5210cda350d341
Pepijn Kenter <titusjan@gmail.com>
2014-06-28 17:14:49 +0200

    main function rainbow map

--- shading_example.py
+++ shading_example.py
@@ -8,23 +8,34 @@ from matplotlib.colors import LightSource
 # or Generic Mapping Tools
 # (http://gmt.soest.hawaii.edu/gmt/doc/gmt/html/GMT_Docs/node145.html)
 
-# test data
-X, Y = np.mgrid[-5:5:0.05, -5:5:0.05]
-Z = np.sqrt(X ** 2 + Y ** 2) + np.sin(X ** 2 + Y ** 2)
-# create light source object.
-ls = LightSource(azdeg=0, altdeg=65)
-# shade data, creating an rgb array.
-rgb = ls.shade(Z, plt.cm.copper)
-# plot un-shaded and shaded images.
-plt.figure(figsize=(12, 5))
-plt.subplot(121)
-plt.imshow(Z, cmap=plt.cm.copper)
-plt.title('imshow')
-plt.xticks([])
-plt.yticks([])
-plt.subplot(122)
-plt.imshow(rgb)
-plt.title('imshow with shading')
-plt.xticks([])
-plt.yticks([])
-plt.show()
+
+def main():
+    # test data
+    x, y = np.mgrid[-5:5:0.05, -5:5:0.05]
+    z = np.sqrt(x ** 2 + y ** 2) + np.sin(x ** 2 + y ** 2)
+    
+    # create light source object.
+    ls = LightSource(azdeg=0, altdeg=65)
+    
+    # shade data, creating an rgb array.
+    cmap = plt.cm.rainbow
+    rgb = ls.shade(z, cmap=cmap)
+    
+    # plot un-shaded and shaded images.
+    plt.figure(figsize=(12, 5))
+    plt.subplot(121)
+    plt.imshow(z, cmap)
+    plt.title('imshow')
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(122)
+    plt.imshow(rgb)
+    plt.title('imshow with shading')
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+    raw_input('please press enter\n')
+    
