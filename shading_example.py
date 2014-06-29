ea5522d90e239ee59a18929ccf53e2117ae48045
Pepijn Kenter <titusjan@gmail.com>
2014-06-29 12:22:19 +0200

    Using axes objects instead of pyplot routines.

--- shading_example.py
+++ shading_example.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LightSource
 
+# From:
+# http://matplotlib.org/examples/pylab_examples/shading_example.html?highlight=codex%20shade
+
+# Look at: 
+# http://rnovitsky.blogspot.nl/2010/04/using-hillshade-image-as-intensity.html
+
 # example showing how to make shaded relief plots
 # like Mathematica
 # (http://reference.wolfram.com/mathematica/ref/ReliefPlot.html)
@@ -21,19 +27,23 @@ def main():
     cmap = plt.cm.rainbow
     rgb = ls.shade(z, cmap=cmap)
     
-    # plot un-shaded and shaded images.
-    plt.figure(figsize=(12, 5))
-    plt.subplot(121)
-    plt.imshow(z, cmap)
-    plt.title('imshow')
-    plt.xticks([])
-    plt.yticks([])
-    plt.subplot(122)
-    plt.imshow(rgb)
-    plt.title('imshow with shading')
-    plt.xticks([])
-    plt.yticks([])
-    plt.show()
+    fig, axes_list = plt.subplots(1, 2, figsize=(12, 5))
+    
+    # No hill shading
+    axes = axes_list[0]
+    axes.imshow(z, cmap)
+    axes.set_title('imshow')
+    axes.set_xticks([])
+    axes.set_yticks([])
+    
+    # Matplotlib default hill shading
+    axes = axes_list[1]
+    axes.imshow(rgb)
+    axes.set_title('imshow with shading')
+    axes.set_xticks([])
+    axes.set_yticks([])    
+    
+    fig.show()
 
 if __name__ == "__main__":
     main()
