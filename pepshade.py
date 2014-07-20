002e347ec5208bfb2c62bcf31efc7bebc25e5932
Pepijn Kenter <titusjan@gmail.com>
2014-07-20 15:01:08 +0200

    Added my own implementation of shading. Uses the novitsky intensity (for now?) but uses it by converting the RGB colors to HSV, merges the intensity into the V channel and then converts back to RGB.

---
+++ pepshade.py
@@ -0,0 +1,59 @@
+""" Hill shading implementation for matplotlib
+
+    TODO:
+        clean up hill_shade_intensity
+        allow hill_shade to accept separate data for the color
+    
+"""
+import matplotlib as mpl
+
+from numpy import pi, cos, sin, gradient, arctan, hypot, arctan2
+from matplotlib import cm
+from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
+
+DEF_AZIMUTH = 165   # degrees
+DEF_ELEVATION = 45  # degrees
+    
+def hill_shade(data, scale=0.1, 
+               azimuth=DEF_AZIMUTH, elevation=DEF_ELEVATION, 
+               cmap=cm.jet):
+    """ Calculates hillshading
+    """
+    assert data.ndim == 2, "data must be 2 dimensional"
+    intensity = hill_shade_intensity(data, scale=scale, 
+                                     azimuth=azimuth, elevation=elevation)
+    normalize_fn = mpl.colors.Normalize()
+    norm_data = normalize_fn(data)
+    rgba = cmap(norm_data)
+    hsv = rgb_to_hsv(rgba[:, :, :3])
+    hsv[:, :, 2] = intensity
+    return hsv_to_rgb(hsv)
+    
+
+    
+# Same as novitsky hill shading but where the gradient is
+# multiplied by the scale instead of divided.
+def hill_shade_intensity(data, scale=0.1, 
+                         azimuth=DEF_AZIMUTH, elevation=DEF_ELEVATION):
+    ''' convert data to hillshade based on matplotlib.colors.LightSource class.
+      input:
+           data - a 2-d array of data
+           scale - scaling value of the data. higher number = lower gradient
+           azdeg - where the light comes from: 0 south ; 90 east ; 180 north ;
+                        270 west
+           altdeg - where the light comes from: 0 horison ; 90 zenith
+      output: a 2-d array of normalized hilshade
+    '''
+    # convert alt, az to radians
+    az = azimuth * pi / 180.0
+    alt = elevation * pi / 180.0
+    # gradient in x and y directions
+    dx, dy = gradient(data * scale)
+    slope = 0.5 * pi - arctan(hypot(dx, dy))
+    aspect = arctan2(dx, dy)
+    intensity = sin(alt) * sin(slope) + cos(alt) * \
+        cos(slope) * cos(-az - aspect - 0.5 * pi)
+    intensity = (intensity - intensity.min()) / \
+        (intensity.max() - intensity.min())
+    return intensity
+
--- shading_example.py
+++ shading_example.py
@@ -19,11 +19,12 @@ For a list of colormaps:
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from matplotlib import cm
 from matplotlib.colors import LightSource
-
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from novitsky import set_shade, hillshade
+from pepshade import hill_shade, hill_shade_intensity
 
 DEF_AZI = 210.0 # default azimuth angle in degrees
 #DEF_AZI = 315.0 # default azimuth angle in degrees
@@ -52,7 +53,7 @@ def mpl_hill_shading(fig, axes, data, cmap, azdeg=DEF_AZI, altdeg=DEF_ALT):
     
     # Norm will not be used to normalize rgb in in the imshow() call but 
     # is used to normalize the color bar. An alternative is to use ColorbarBase
-    # as is demonstrated in novitsky_hill_shading below.
+    # as is demonstrated in novitsky_hill_shading below
     image = axes.imshow(rgb, cmap, norm=norm)
     axes.set_title('Matplotlib hill shading')
     axes.set_xticks([])
@@ -81,14 +82,19 @@ def novitsky_hill_shading(fig, axes, data, cmap,
     colorbar = mpl.colorbar.ColorbarBase(colorbar_axes, cmap=cmap, norm=norm, orientation='vertical')
 
 
-def novitsky_intensity(fig, axes, data, cmap, 
+def intensity(fig, axes, data, cmap, 
                           azdeg=DEF_AZI, altdeg=DEF_ALT, scale = DEF_SCALE):
     " Shows only the shading component of the shading by Ran Novitsky"
     # Intensity will always be between 0 and 1
-    intensity = hillshade(data, azdeg=azdeg, altdeg=altdeg, scale = scale)
+    
+    if 0:
+        intensity = hillshade(data, azdeg=azdeg, altdeg=altdeg, scale = scale)
+        axes.set_title('Ran Novitsky intensity')
+    else:    
+        intensity = hill_shade_intensity(data, azdeg=azdeg, altdeg=altdeg, scale = scale)
+        axes.set_title('Pepijn Kenter intensity')
+    
     image = axes.imshow(intensity, cmap)
-
-    axes.set_title('Ran Novitsky intensity')
     axes.set_xticks([])
     axes.set_yticks([])    
     
@@ -97,27 +103,51 @@ def novitsky_intensity(fig, axes, data, cmap,
     colorbar = fig.colorbar(image, cax=colorbar_axes, orientation='vertical')
 
 
+def kenter_hill_shading(fig, axes, data, cmap, 
+                        azdeg=DEF_AZI, altdeg=DEF_ALT, scale = DEF_SCALE):
+    " Shows data with hill shading by Ran Novitsky"
+
+    norm = mpl.colors.Normalize(vmin=np.min(data), vmax=np.max(data))
+    
+    rgb = hill_shade(data, cmap=cmap, azimuth=azdeg, elevation=altdeg, scale = scale)
+    image = axes.imshow(rgb)
+
+    axes.set_title('Pepijn Kenter hill shading')
+    axes.set_xticks([])
+    axes.set_yticks([])    
+    
+    divider = make_axes_locatable(axes)    
+    colorbar_axes = divider.append_axes('right', size="5%", pad=0.25, add_to_figure=True)
+    colorbar = mpl.colorbar.ColorbarBase(colorbar_axes, cmap=cmap, norm=norm, orientation='vertical')
+
+
 def main():
     # test data
     x, y = np.mgrid[-5:5:0.05, -5:5:0.05]
     data = np.sqrt(x ** 2 + y ** 2) + np.sin(x ** 2 + y ** 2)
     #cmap = plt.cm.rainbow
-    cmap = plt.cm.cool
+    #cmap = plt.cm.cool
+    cmap = plt.cm.jet
     
+    #from objbrowser import browse
+    #browse(cmap(0, 1), "cmap")
+    #rgb = hillshade(data, cmap=cmap, scale=10)
+    #import sys; sys.exit()
     # shade data, creating an rgb array.
     
-    fig, axes_list = plt.subplots(2, 2, figsize=(12, 12))
+    fig, axes_list = plt.subplots(2, 2, figsize=(10, 10))
     
     if 1:
-        no_shading           (fig, axes_list[0, 0], data, cmap)
-        mpl_hill_shading     (fig, axes_list[0, 1], data, cmap)
-        novitsky_hill_shading(fig, axes_list[1, 0], data, cmap, scale = 0.1)
-        novitsky_intensity   (fig, axes_list[1, 1], data, plt.cm.gist_gray, scale = 0.1)
+        no_shading            (fig, axes_list[0, 0], data, cmap)
+        mpl_hill_shading      (fig, axes_list[0, 1], data, cmap)
+        novitsky_hill_shading (fig, axes_list[1, 0], data, cmap, scale = 10)
+        #novitsky_intensity   (fig, axes_list[1, 1], data, plt.cm.gist_gray, scale = 10)
+        kenter_hill_shading   (fig, axes_list[1, 1], data, cmap, scale = 10)
     else:
-        novitsky_intensity   (fig, axes_list[0, 0], data, plt.cm.gist_gray, scale = 0.1)
-        novitsky_intensity   (fig, axes_list[0, 1], data, plt.cm.gist_gray, scale = 0.2)
-        novitsky_intensity   (fig, axes_list[1, 0], data, plt.cm.gist_gray, scale = 0.5)
-        novitsky_intensity   (fig, axes_list[1, 1], data, plt.cm.gist_gray, scale = 0.001)
+        intensity   (fig, axes_list[0, 0], data, plt.cm.gist_gray, scale = 10)
+        intensity   (fig, axes_list[0, 1], data, plt.cm.gist_gray, scale = 5)
+        intensity   (fig, axes_list[1, 0], data, plt.cm.gist_gray, scale = 2)
+        intensity   (fig, axes_list[1, 1], data, plt.cm.gist_gray, scale = 1000)
 
     fig.show()
 
